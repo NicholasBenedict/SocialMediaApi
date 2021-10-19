48a0b89b42b9956b2cb714705ae0ae31552e4f73
Nicholas Benedict <nicholasdbenedict@gmail.com>
2021-10-19 14:41:49 -0400

    added post comment and reply classes

---
+++ SocialMediaApi.Data/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApi.Data
+{
+    public class Comment
+    {
+    }
+}
